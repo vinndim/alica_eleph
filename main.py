09a57f2d0d9cb5aec69f62174e74f3afa91eeeaf
vinndim <vinnikov.dima2005@gmail.com>
2022-03-15 19:20:40 +0500

    add files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # импортируем библиотеки
+import os
+
 from flask import Flask, request
 import logging
 
@@ -132,4 +134,6 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
